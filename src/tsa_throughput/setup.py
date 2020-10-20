d2b21a9c1a687a1a00edefdc9f985617a8ac147a
Shaylyn Karan <Shaylyn.Karan@alaskaair.com>
2020-10-20 15:49:56 -0700

    added python project using data science template

---
+++ src/tsa_throughput/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A fun project to analyze TSA Throughput Data',
+    author='alaska',
+    license='',
+)
