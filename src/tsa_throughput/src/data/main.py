71be212414af4e9f9266461e75ee89b20d90ede5
Shaylyn Karan <Shaylyn.Karan@alaskaair.com>
2020-11-02 10:26:37 -0800

    flattens json completely and exports as csv

--- src/tsa_throughput/src/data/main.py
+++ src/tsa_throughput/src/data/main.py
@@ -5,10 +5,21 @@ from pandas import json_normalize
 
 #load json file
 with open(r"c:\users\skaran\repos\tsathroughput\data\tsathroughput.json") as f:
-  data = json.load(f)
+    data = json.load(f)
 
-#normalize json
-df = json_normalize(data)
+#normalize and store in df
+df = pandas.json_normalize(data,
+                       record_path=['Airports', 'Days', 'Checkpoints', 'Hours'],
+                       meta=[
+                           ['Airports', 'Days', 'Checkpoints', 'CheckpointName'],
+                           ['Airports', 'Days', 'Date'],
+                           ['Airports', 'AirportCode'],
+                           ['Airports', 'AirportName'],
+                           ['Airports', 'City'],
+                           ['Airports', 'State'],
+                       ],
+                       )
+print(df)
 
 #export normalized JSON to CSV file
-csv_export = df.to_csv('tsathroughputsmall.csv', index=False)
+csv_export = df.to_csv(r"c:\users\skaran\repos\tsathroughput\data\tsathroughputsmall.csv", index=False)
