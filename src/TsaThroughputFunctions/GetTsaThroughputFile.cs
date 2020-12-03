d62654c2b4bb6319576964e195d7c1ebd0f99a30
Shaylyn Karan <Shaylyn.Karan@alaskaair.com>
2020-12-03 11:01:03 -0800

    added htmlagilitypack to load web page

--- src/TsaThroughputFunctions/GetTsaThroughputFile.cs
+++ src/TsaThroughputFunctions/GetTsaThroughputFile.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
@@ -8,9 +9,42 @@ namespace TsaThroughput.Data.Raw
     public static class GetTsaThroughputFile
     {
         [FunctionName("GetTsaThroughputFile")]
-        public static void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
+        public static void Run([TimerTrigger("0 * */30 * * *")]TimerInfo myTimer, ILogger log)
         {
+            string s = await GetlatestFiles();
+            log.LogInforamtion($"File: {s}");
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+        }
+        
+        public static async Task<string> GetLatestFiles() {
+            var latestFile = await GetlatestFile();
+
+            return latestFile;
+        }
+
+        public static async Task<string> GetlatestFile() {
+            string website = "https://www.tsa.gov/foia/readingroom";
+
+            HttpClient client = new HttpClient();
+            string html = await client.GetStringAsync(website);
+
+            HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(html); 
+
+            var file = doc.DocumentNode
+                .SelectNodes("//a")
+                .FirstOrDefault(x => x.Attributes.Contains("class") && x.Attributes["class"].Value.Contains("foia-reading-link"));
+
+            var url = file
+                .Attributes
+                .FirstOrDefault(x => x.Name == "href")
+                .Value;
+
+            var title = file
+                .SelectSingleNode("//td/a")
+                .InnerHtml;
+
+            return $"<p><a href=\"{website}{url}\">{title}</a></p>";
         }
     }
 }
