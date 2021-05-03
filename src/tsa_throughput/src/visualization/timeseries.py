a9401865b59033204483a3508278dd271299cf7b
Shaylyn Karan <Shaylyn.Karan@alaskaair.com>
2021-05-03 08:42:12 -0700

    Working through time series

--- src/tsa_throughput/src/visualization/timeseries.py
+++ src/tsa_throughput/src/visualization/timeseries.py
@@ -80,3 +80,44 @@ rolling_mean = df_log.rolling(window=12).mean()
 df_log_minus_mean = df_log - rolling_mean
 df_log_minus_mean.dropna(inplace=True)
 get_stationarity(df_log_minus_mean)
+
+
+p = d = q = range(0, 2)
+pdq = list(itertools.product(p, d, q))
+seasonal_pdq = [(x[0], x[1], x[2], 12) for x in list(itertools.product(p, d, q))]
+print('Examples of parameter combinations for Seasonal ARIMA...')
+print('SARIMAX: {} x {}'.format(pdq[1], seasonal_pdq[1]))
+print('SARIMAX: {} x {}'.format(pdq[1], seasonal_pdq[2]))
+print('SARIMAX: {} x {}'.format(pdq[2], seasonal_pdq[3]))
+print('SARIMAX: {} x {}'.format(pdq[2], seasonal_pdq[4]))
+
+#Grid search to find the optimal set of parameters for model
+
+# for param in pdq:
+#     for param_seasonal in seasonal_pdq:
+#         try:
+#             mod = sm.tsa.statespace.SARIMAX(dfg,
+#                                             order=param,
+#                                             seasonal_order=param_seasonal,
+#                                             enforce_stationarity=False,
+#                                             enforce_invertibility=False)
+
+#             results = mod.fit()
+
+#             print('ARIMA{}x{}12 - AIC:{}'.format(param, param_seasonal, results.aic))
+#         except:
+#             continue
+
+mod = sm.tsa.statespace.SARIMAX(dfg,
+                                order=(1, 1, 1),
+                                seasonal_order=(1, 0, 1, 12),
+                                enforce_stationarity=False,
+                                enforce_invertibility=False)
+
+results = mod.fit()
+
+print(results.summary().tables[1])
+
+#Diagnostics to check for anything unusual
+results.plot_diagnostics(figsize=(16, 8))
+plt.show()
