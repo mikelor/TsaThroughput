ed8a0ac171b29127a6c8815cdbedb0f958fe6a39
mikelor <mikelor@gmail.com>
2021-07-01 10:21:50 -0700

    Added BOI, BZN, MSO

--- scripts/processJsonToCsv.sh
+++ scripts/processJsonToCsv.sh
@@ -1,12 +1,15 @@
 python ../src/tsa_throughput/src/data/make_airport_dataset.py dir -d ../data/raw/tsa/throughput                                      -o ../data/processed/tsa/throughput
 python ../src/tsa_throughput/src/data/make_airport_dataset.py csv -f ../data/processed/tsa/throughput/TsaThroughput.Cache.csv -a ANC -o ../data/processed/tsa/throughput 
 python ../src/tsa_throughput/src/data/make_airport_dataset.py csv -f ../data/processed/tsa/throughput/TsaThroughput.Cache.csv -a ATL -o ../data/processed/tsa/throughput
+python ../src/tsa_throughput/src/data/make_airport_dataset.py csv -f ../data/processed/tsa/throughput/TsaThroughput.Cache.csv -a BOI -o ../data/processed/tsa/throughput      
+python ../src/tsa_throughput/src/data/make_airport_dataset.py csv -f ../data/processed/tsa/throughput/TsaThroughput.Cache.csv -a BZN -o ../data/processed/tsa/throughput      
 python ../src/tsa_throughput/src/data/make_airport_dataset.py csv -f ../data/processed/tsa/throughput/TsaThroughput.Cache.csv -a DEN -o ../data/processed/tsa/throughput      
 python ../src/tsa_throughput/src/data/make_airport_dataset.py csv -f ../data/processed/tsa/throughput/TsaThroughput.Cache.csv -a DFW -o ../data/processed/tsa/throughput 
 python ../src/tsa_throughput/src/data/make_airport_dataset.py csv -f ../data/processed/tsa/throughput/TsaThroughput.Cache.csv -a FLL -o ../data/processed/tsa/throughput
 python ../src/tsa_throughput/src/data/make_airport_dataset.py csv -f ../data/processed/tsa/throughput/TsaThroughput.Cache.csv -a LAS -o ../data/processed/tsa/throughput 
 python ../src/tsa_throughput/src/data/make_airport_dataset.py csv -f ../data/processed/tsa/throughput/TsaThroughput.Cache.csv -a LAX -o ../data/processed/tsa/throughput 
 python ../src/tsa_throughput/src/data/make_airport_dataset.py csv -f ../data/processed/tsa/throughput/TsaThroughput.Cache.csv -a MIA -o ../data/processed/tsa/throughput 
+python ../src/tsa_throughput/src/data/make_airport_dataset.py csv -f ../data/processed/tsa/throughput/TsaThroughput.Cache.csv -a MSO -o ../data/processed/tsa/throughput      
 python ../src/tsa_throughput/src/data/make_airport_dataset.py csv -f ../data/processed/tsa/throughput/TsaThroughput.Cache.csv -a MCO -o ../data/processed/tsa/throughput
 python ../src/tsa_throughput/src/data/make_airport_dataset.py csv -f ../data/processed/tsa/throughput/TsaThroughput.Cache.csv -a PDX -o ../data/processed/tsa/throughput 
 python ../src/tsa_throughput/src/data/make_airport_dataset.py csv -f ../data/processed/tsa/throughput/TsaThroughput.Cache.csv -a PHX -o ../data/processed/tsa/throughput 
