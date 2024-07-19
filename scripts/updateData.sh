2847eff413b7bdf8d116eb0fb2a794ec7b3465a2
mikelor <mikelor@gmail.com>
2024-07-19 13:37:47 -0700

    Updated Scripts

--- scripts/updateData.sh
+++ scripts/updateData.sh
@@ -1,5 +1,5 @@
 echo $1
-#./cvtPdfToJson.sh $1
-python ../src/tsa_throughput/src/data/make_airport_dataset.py file -f ../data/raw/tsa/throughput/$1 -o ../data/processed/tsa/throughput/
-./processJsonToCsv.sh
-./createFigures.sh
+./cvtPdfToJson.sh $1
+./cvtJsonToCsv.sh $1
+./createAirportCsvs.sh
+./createAirportFigures.sh
